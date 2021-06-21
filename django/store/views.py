239ab87151faf8f7bd31edd0f1fa73a1a6cf0637
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-06-21 09:28:50 +0530

    initial commit

---
+++ django/store/views.py
@@ -0,0 +1,35 @@
+from django.urls.conf import include
+from store import serializers
+from store.serializers import ProductSerializer
+from store.models import Product
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Category, Product
+from store import models
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class Product(generics.RetrieveAPIView):
+    lookup_field = "slug"
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryItemView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.filter(
+            category__in=Category.objects.get(slug=self.kwargs["slug"]).get_descendants(include_self=True)
+        )
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
+
+
